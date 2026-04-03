6c9316a76bb9ce46fd3188a03514c7cdf9c15b26
Anzyll <anzil2003m@gmail.com>
2026-04-03 12:24:53 +0530

    chore: setup Spring Boot project,base structure and database schema

---
+++ src/main/java/com/anzil/finlytics/FinlyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anzil.finlytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlyticsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinlyticsBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(150) UNIQUE NOT NULL,
+                       role VARCHAR(20) NOT NULL CHECK (role IN ('ADMIN', 'ANALYST', 'VIEWER')),
+                       status VARCHAR(20) DEFAULT 'ACTIVE',
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+                            id BIGSERIAL PRIMARY KEY,
+                            name VARCHAR(100) NOT NULL,
+                            type VARCHAR(20) NOT NULL CHECK (type IN ('INCOME', 'EXPENSE'))
+);
+
+CREATE TABLE financial_records (
+                                   id BIGSERIAL PRIMARY KEY,
+                                   user_id BIGINT NOT NULL,
+                                   category_id BIGINT,
+                                   amount NUMERIC(10,2) NOT NULL,
+                                   type VARCHAR(20) NOT NULL CHECK (type IN ('INCOME', 'EXPENSE')),
+                                   date DATE NOT NULL,
+                                   notes TEXT,
+                                   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+                                   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                                   FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE audit_logs (
+                            id BIGSERIAL PRIMARY KEY,
+                            user_id BIGINT,
+                            action VARCHAR(50) NOT NULL,
+                            entity_type VARCHAR(50),
+                            entity_id BIGINT,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+                            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
+
